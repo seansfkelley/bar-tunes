368c6fac6112ebae7e22158a395cf9a43e246cce
seansfkelley <seansfkelley@gmail.com>
2012-07-24 02:07:03 -0700

    Some todo changes.

--- Song Ticker/AppDelegate.h
+++ Song Ticker/AppDelegate.h
@@ -42,23 +42,25 @@ typedef enum player {
 @end
 
 /*
-To do:
- how to handle crash/force-quit of application we're listening to?
- why is itunes being kept open?
+To do (ordered by approximate priority):
  format popup should 
     listen to esc as cancel
     be closed when the menu is opened
- scroll while menu is selected?
- fade at edges?
+ save format string/app selections
+ implement any-player option (what is appropriate logic for this: last player to send play notification [that is NOT overridden by a pause]?)
  force to be always leftmost
  menu options
     [alternate] show icon on [left/right]
     slider: scroll speed (?)
     checkbox: show text when paused
     checkbox: scroll around v. scroll side-to-side
- what's the CPU usage like? unreasonable? at least it doesn't use the dedicated GPU
- scroll speed constant, or a function of the length of the text?
- app icon
- save format string/app selections
  MVC separation -- controllers = views = models now (format, menu handlers)
+ app icon
+
+Questions:
+ what's the CPU usage like? unreasonable? at least it doesn't use the dedicated GPU
+ how to handle crash/force-quit of application we're listening to?
+ why is itunes being kept open?
+ scroll while menu is selected?
+ scroll speed constant, or a function of the length of the text
 */
